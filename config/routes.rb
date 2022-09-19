bcd4b6681efc4caafa57fafd8a7fb910a5178ff2
Baber Javed <baberjaved.rs@gmail.com>
2022-09-19 17:32:30 +0500

    Added Login functionality and Groups index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+    root to: "devise/sessions#new"
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :groups
 
   # Defines the root path route ("/")
-  root "groups#index"
 end
